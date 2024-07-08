c7d0ae441b90bcd87856726c77f0cf302a1a8385
gangyeol.kim <kkyy3402@naver.com>
2024-07-09 00:37:26 +0900

    feat: practice

--- main.ts
+++ main.ts
@@ -1,85 +1,54 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import {App, Editor, MarkdownView, Modal, Plugin, PluginSettingTab, Setting} from 'obsidian';
 
 interface MyPluginSettings {
-	mySetting: string;
+	apiKey: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	apiKey: ''
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
+		console.info("HELLO!")
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Command to trigger GPT API
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			id: 'trigger-gpt-api',
+			name: 'Trigger GPT API',
+			hotkeys: [
+				{
+					modifiers: ["Ctrl"],
+					key: "Enter",
 				}
+			],
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				if (!this.settings.apiKey) {
+					new SettingModal(this.app, this).open();
+					return;
+				}
+				const selection = editor.getSelection();
+
+				let prompt = editor.getSelection();
+				if (!prompt) {
+					prompt = editor.getValue();  // Get the entire content if nothing is selected
+				}
+
+				console.info(`value : ${editor.getValue()}`)
+
+				const response = await this.callGptApi(selection);
+				editor.replaceSelection(response);
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Add settings tab
+		this.addSettingTab(new SettingTab(this.app, this));
 	}
 
 	onunload() {
-
 	}
 
 	async loadSettings() {
@@ -89,16 +58,56 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async callGptApi(prompt: string): Promise<string> {
+		const apiKey = this.settings.apiKey;
+		console.log('Calling GPT-4 API with prompt:', prompt);
+
+		const response = await fetch('https://api.openai.com/v1/completions', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': `Bearer ${apiKey}`
+			},
+			body: JSON.stringify({
+				model: 'gpt-4o',
+				prompt: prompt,
+				max_tokens: 150
+			})
+		});
+
+		if (!response.ok) {
+			console.error('API call failed:', response.statusText);
+			throw new Error('API call failed');
+		}
+
+		const data = await response.json();
+		console.log('API response:', data);
+		return data.choices[0].text.trim();
+	}
+
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class SettingModal extends Modal {
+	plugin: MyPlugin;
+
+	constructor(app: App, plugin: MyPlugin) {
 		super(app);
+		this.plugin = plugin;
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		contentEl.setText('API key is not set. Please go to the settings to configure your GPT API key.');
+
+		// Add a div for spacing
+		contentEl.createEl('br');
+
+		const buttonEl = contentEl.createEl('button', {text: 'Go to Settings'});
+		buttonEl.onclick = () => {
+			this.plugin.addSettingTab(new SettingTab(this.app, this.plugin));
+			this.close();
+		}
 	}
 
 	onClose() {
@@ -107,7 +116,7 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
+class SettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -121,13 +130,13 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('GPT API Key')
+			.setDesc('Enter your GPT API key here')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your API key')
+				.setValue(this.plugin.settings.apiKey)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.apiKey = value;
 					await this.plugin.saveSettings();
 				}));
 	}
